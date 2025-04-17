c4c32adc4e827646fd92ace919a0e5c117607426
dj.ibragimova <dj.ibragimova@gmail.com>
2025-04-17 15:16:50 +0700

    🌟 Первый коммит: структура проекта, pom.xml и Hello Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Джамиля! Добро пожаловать в мир автоматизации на Java 🌌");
+    }
+}
